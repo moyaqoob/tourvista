45f3e60e61436925f3864642685f6ca85edd78ec
moyaqoob <moyaqoob28@gmail.com>
2025-07-24 11:08:02 +0530

    Add

--- app/auth/client.ts
+++ app/auth/client.ts
@@ -1,14 +1,14 @@
 import { Account, Client, Databases, Storage } from "appwrite";
 const appwriteConfig = {
-  projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID || " ",
-  auth: import.meta.env.VITE_APPWRITE_AUTH_KEY || "",
-  api_secret: import.meta.env.VITE_API_SECRET || "",
-  databaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID || " ",
-  userscollection: import.meta.env.VITE_APPWRITE_USERS_COLLECTION || " ",
-  tripsCollection: import.meta.env.VITE_APPWRITE_TRIPS_COLLECTION || " ",
-  endpointUrl: import.meta.env.VITE_APPWRITE_URL || "NOT found ",
-  geminiKey: import.meta.env.VITE_GEMINI_KEY,
-  unsplashKey: import.meta.env.VITE_UNSPLASH_KEY,
+  projectId: process.env.VITE_APPWRITE_PROJECT_ID || " ",
+  auth: process.env.VITE_APPWRITE_AUTH_KEY || "",
+  api_secret: process.env.VITE_API_SECRET || "",
+  databaseId: process.env.VITE_APPWRITE_DATABASE_ID || " ",
+  userscollection: process.env.VITE_APPWRITE_USERS_COLLECTION || " ",
+  tripsCollection: process.env.VITE_APPWRITE_TRIPS_COLLECTION || " ",
+  endpointUrl: process.env.VITE_APPWRITE_URL || "NOT found ",
+  geminiKey: process.env.VITE_GEMINI_KEY,
+  unsplashKey: process.env.VITE_UNSPLASH_KEY,
 };
 
 const client = new Client()
